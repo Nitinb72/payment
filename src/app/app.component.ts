2ab5e895f07ddfc118abcfea1539793af233d581
Nitinb72 <nitinb314@gmail.com>
2022-12-09 11:58:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'payment';
+
+receiver_name: string=''
+userInput(event: any){
+  this.receiver_name = event.target.value;
+  console.log(this.receiver_name)
 }
+payment_date: string=''
+userInput1(event:any){
+  this.payment_date = event.target.value;
+  console.log(this.payment_date)
+}
+
+total_amount: string=''
+userInput2(event:any){
+  this.total_amount = event.target.value
+}
+}
+ /* displayName='';
+  displayDate='';
+  displayAmt='';
+
+  getName(name:any){
+   console.warn(name.target.value)
+    this.displayName=name.target.value
+  }
+
+  getDate(date:any){
+    //console.log(date.target.value)
+    this.displayDate=date.target.value
+  }
+
+  getAmt(amt:any){
+    console.warn(amt.target.value)
+    this.displayAmt=amt.target.value
+  }
+*/
